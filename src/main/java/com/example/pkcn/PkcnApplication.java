d15396a62252e160da66e84fe3d4832f41638231
NguyenTruongVu <nguyentruongvu000111@gmail.com>
2026-05-29 13:29:36 +0700

    Tạo các phụ thuộc, cấu trúc thư mục dự án

---
+++ src/main/java/com/example/pkcn/PkcnApplication.java
@@ -0,0 +1,13 @@
+package com.example.pkcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkcnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkcnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pkcn/PkcnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pkcn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkcnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
