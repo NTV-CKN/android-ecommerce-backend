de8425b8b7a9af5bd2a6ce74d7b36570c3e58bcd
JollyHuynh <23130323@st.hcmuaf.edu.vn>
2026-06-30 14:26:04 +0700

    product_manager

--- src/main/java/com/example/pkcn/controller/order/OrderController.java
+++ src/main/java/com/example/pkcn/controller/order/OrderController.java
@@ -1,4 +1,14 @@
 package com.example.pkcn.controller.order;
 
+import com.example.pkcn.dto.response.OrderManageDTO;
+import com.example.pkcn.service.order.IOrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.pkcn.dto.request.UpdateOrderStatusRequest;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/order")
 public class OrderController {
+
 }
