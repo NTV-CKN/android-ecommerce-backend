e725517f51af98ea80e2921427da5c5f55c153a3
NguyenTruongVu <nguyentruongvu000111@gmail.com>
2026-07-10 01:16:57 +0700

    Thêm logic xử lí xóa biến thể khỏi sản phẩm

--- src/main/java/com/example/pkcn/service/admin/product/IProductAdminService.java
+++ src/main/java/com/example/pkcn/service/admin/product/IProductAdminService.java
@@ -2,12 +2,12 @@ package com.example.pkcn.service.admin.product;
 
 import com.example.pkcn.dto.ProductAdminPageDTO;
 import com.example.pkcn.dto.response.PageResponseDTO;
+import com.example.pkcn.dto.response.ProductVariantDTO;
 import com.example.pkcn.dto.response.SuccessBasicDTO;
 
 public interface IProductAdminService {
     PageResponseDTO<ProductAdminPageDTO> getProducts(String keyWord, Integer page, Integer pageSize);
-
     String generateUniqueSku(String productName, String color, String size);
-
     SuccessBasicDTO saveProduct(ProductAdminPageDTO productAdminPageDTO);
+    SuccessBasicDTO removeVariant(ProductVariantDTO productVariantDTO);
 }
